1856861de283bc80fa6474464c3c8348070ee25f
Rob Maes <robert.maes@smoothstack.com>
2021-03-17 11:39:59 -0500

    hotfix: link shared library with maven

--- src/main/java/com/smoothstack/utopia/ticketpaymentservice/TicketPaymentServiceApplication.java
+++ src/main/java/com/smoothstack/utopia/ticketpaymentservice/TicketPaymentServiceApplication.java
@@ -1,5 +1,6 @@
 package com.smoothstack.utopia.ticketpaymentservice;
 
+import com.smoothstack.utopia.shared.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
